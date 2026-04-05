670a77031453f3bcb386a6b5855ead6030d7d96d
Sri Manish <magicofmathsislogic@gmail.com>
2026-04-05 22:42:58 +0530

    add new file

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-    base: '/finVault/'   // 👈 MUST match repo name exactly
+    base: '/financialDashboard/'   // 👈 MUST match repo name exactly
 
 })
